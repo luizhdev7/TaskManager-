dc8a6407ae7b46753e8a4f060fb39b09697757d6
luizh.dev7 <luizh.dev7@gmail.com>
2026-04-12 12:28:12 -0300

    fix input validation loop in TaskManager

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 package main;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("Enter task title: ");
+            String title = scanner.nextLine();
+
+            if (title.matches(".*[a-zA-Z].*")) {
+                System.out.println("You created the task: " + title);
+                break;
+            } else {
+                System.out.println("Invalid title! Must contain at least one letter.");
+            }
+        }
 
     }
-
 }
