844c15654dacb84fb771ec194803d680eafdbd04
luizh.dev7 <luizh.dev7@gmail.com>
2026-04-13 22:52:18 -0300

    create task feature with validation flow

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,23 +2,68 @@ package main;
 
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.print("Enter task title: ");
-            String title = scanner.nextLine();
+        int option;
 
-            if (title.matches(".*[a-zA-Z].*")) {
-                System.out.println("You created the task: " + title);
-                break;
-            } else {
-                System.out.println("Invalid title! Must contain at least one letter.");
+        while (true) {
+            System.out.println("Select a option:");
+            System.out.print("1 - Create new task \n" +
+                    "2 - List tasks \n" +
+                    "3 - Delete task \n");
+
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            if (option == 1) {
+                System.out.println("Create selected");
+
+                while (true) {
+
+                    String title;
+
+                    while (true) {
+                        System.out.println("Enter task title:");
+                        title = scanner.nextLine();
+
+                        if (!title.matches(".*[a-zA-Z].*")) {
+                            System.out.println("Invalid title! Must contain at least one letter.");
+                        } else {
+                            break;
+                        }
+                    }
+
+                    System.out.println("You created the task: " + title);
+
+                    System.out.println("Do you want to create another task? (y/n)");
+                    String again = scanner.nextLine();
+
+                    if (!again.equalsIgnoreCase("y")) {
+                        break;
+                    }
+                }
+            }
+
+
+            if (option == 2) {
+                System.out.println("List selected");
+            }
+
+            if (option == 3) {
+                System.out.println("Delete selected");
             }
         }
-
     }
 }
+
+
+
+
+
+
+
+
+
